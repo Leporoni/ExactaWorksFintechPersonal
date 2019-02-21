7a35e38568a7361a28f7c23cdf976104dd78dbd7
Leporoni <leporoni@hotmail.com>
2019-02-21 16:47:20 -0300

    first commit

--- src/main/java/br/com/exactaworks/controller/GastoController.java
+++ src/main/java/br/com/exactaworks/controller/GastoController.java
@@ -1,19 +1,21 @@
 package br.com.exactaworks.controller;
 
+import java.util.Optional;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.exactaworks.model.Gasto;
 import br.com.exactaworks.service.GastoService;
 
-@Controller
+@RestController
 public class GastoController {
 
 	@Autowired
@@ -21,7 +23,6 @@ public class GastoController {
 
 	@GetMapping("/")
 	public ModelAndView findAll() {
-
 		ModelAndView mv = new ModelAndView("/gasto");
 		mv.addObject("gastos", service.findAll());
 
@@ -29,17 +30,15 @@ public class GastoController {
 	}
 
 	@GetMapping("/add")
-	public ModelAndView add(Gasto gasto) {
-
+	public ModelAndView add(Optional<Gasto> optional) {
 		ModelAndView mv = new ModelAndView("/gastoAdd");
-		mv.addObject("gasto", gasto);
+		mv.addObject("gasto", optional);
 
 		return mv;
 	}
 
 	@PostMapping("/save")
 	public ModelAndView save(@Valid Gasto gasto, BindingResult result) {
-
 		if (result.hasErrors()) {
 			return add(gasto);
 		}
@@ -51,18 +50,18 @@ public class GastoController {
 
 	@GetMapping("/edit/{id}")
 	public ModelAndView edit(@PathVariable("id") Long id) {
-
-		ModelAndView mv = new ModelAndView("/Add");
-		service.findOne(id);
-		return mv;
+		return add(service.findOne(id));
 	}
 
 	@GetMapping("/delete/{id}")
 	public ModelAndView delete(@PathVariable("id") Long id) {
-
 		service.delete(id);
-
 		return findAll();
+	}
+
+	public ModelAndView add(Gasto gasto) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
--- src/main/java/br/com/exactaworks/model/Gasto.java
+++ src/main/java/br/com/exactaworks/model/Gasto.java
@@ -2,15 +2,13 @@ package br.com.exactaworks.model;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
-import java.time.LocalDate;
+import java.sql.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
-import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 public class Gasto implements Serializable {
@@ -26,29 +24,15 @@ public class Gasto implements Serializable {
 
 	@Column(length = 100)
 	private String descricao;
-
 	
-	@DateTimeFormat(pattern = "dd/MM/yyyy HH:mm")
-	private LocalDate dataHora;
+	
+	private Date dataHora;
 
 	@Column(nullable = false, length = 50)
 	private BigDecimal valor;
 
 	@Column(length = 150)
 	private String tags;
-
-	public Gasto() {
-
-	}
-
-	public Gasto(Long idGasto, String nomePessoa, String descricao, LocalDate dataHora, BigDecimal valor, String tags) {
-		this.idGasto = idGasto;
-		this.nomePessoa = nomePessoa;
-		this.descricao = descricao;
-		this.dataHora = dataHora;
-		this.valor = valor;
-		this.tags = tags;
-	}
 
 	public Long getIdGasto() {
 		return idGasto;
@@ -74,11 +58,11 @@ public class Gasto implements Serializable {
 		this.descricao = descricao;
 	}
 
-	public LocalDate getDataHora() {
+	public Date getDataHora() {
 		return dataHora;
 	}
 
-	public void setDataHora(LocalDate dataHora) {
+	public void setDataHora(Date dataHora) {
 		this.dataHora = dataHora;
 	}
 
@@ -96,37 +80,6 @@ public class Gasto implements Serializable {
 
 	public void setTags(String tags) {
 		this.tags = tags;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((idGasto == null) ? 0 : idGasto.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Gasto other = (Gasto) obj;
-		if (idGasto == null) {
-			if (other.idGasto != null)
-				return false;
-		} else if (!idGasto.equals(other.idGasto))
-			return false;
-		return true;
-	}
-
-	@Override
-	public String toString() {
-		return "Gasto [idGasto=" + idGasto + ", nomePessoa=" + nomePessoa + ", descricao=" + descricao + ", dataHora="
-				+ dataHora + ", valor=" + valor + ", tags=" + tags + "]";
 	}
 
 }
